c904d94a17f3227cdda0c2a03c3543f952ccf856
Kevin Bandison <97281346+Kbandison@users.noreply.github.com>
2024-01-06 23:13:48 +0000

    Added background and hero

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Welcome to Coreride!</h1>
+    <div className="bg-[url(src/images/063-tesla-model-3-2024-review.jpg)] bg-cover ">
+      <div className="h-[95vh] flex flex-col justify-center items-center bg-black bg-opacity-20">
+        <h1 className="text-white">Welcome to Coreride!</h1>
+        <button className="btn">Buy now!</button>
+      </div>
     </div>
   );
 };
